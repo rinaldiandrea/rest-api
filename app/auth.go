3abb42bff17befd2e95caa7a7ee2b7e53d6d25e0
rinaldiandrea <48564077+rinaldiandrea@users.noreply.github.com>
2019-03-22 10:26:52 -0700

    More about JWT

--- app/auth.go
+++ app/auth.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	u "lens/utils"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -31,5 +34,41 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 		}
 
 		splitted := strings.Split(tokenHeader, " ") //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		tokenPart := splitted[1] //Grab the token part, what we are truly interested in
+		tk := &models.Token()
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil { //Malformed toekn, returns with http code 403 as usual
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		if !token.Valid {
+			response = u.Message(false, "Token is not valid.")
+			w.WriterHeader(http.StatusForbidden)
+			w.Header.Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		//Everything went well, proceed with the request and set the caller to the user retrieved from the parsed token
+		fmt.Sprintf("User &", tk.Username) //Useful for monitoring
+		ctx := context.WithValue(r.Context(), "user", tk.UserId)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r) //proceed in the middleware chain!
 	})
 }
